05ad23311a17add399e90f27b5d3bff62fa818d2
Ohtani <72929062+yuka0216@users.noreply.github.com>
2021-06-07 05:30:36 +0900

    テンプレート文字列、アロー関数、分割代入について

--- src/index.js
+++ src/index.js
@@ -44,3 +44,65 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = "ゆか";
+// const age = 28;
+// //「私の名前はゆかです。年齢は28歳です。」
+// //従来の方法
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
+// //テンプレート文字列を用いた方法
+// const message2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+//従来の関数
+// function func1(str) {
+// return str;
+// }
+// const func1 = function (str) {
+//   return str;
+// };
+// console.log(func1("func1です"));
+
+// アロー関数
+//引数のカッコは省略可能だが保存すると保管される
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
+// //処理が一行で終わるときreturnと{}を省略できる。
+// const func3 = (str) => str;
+// console.log(func3("func3です"));
+
+// const func4 = (num1, num2) => num1 + num2;
+// console.log(func4(2, 3));
+
+/**
+ * 分割代入
+ */
+// オブジェクト;
+// const myProfile = {
+//   name: "yuka",
+//   age: 28
+// };
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}歳です`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `名前は${name}です。年齢は${age}す`;
+// console.log(message2);
+
+// 配列;
+const myProfile = ["ゆか", 28];
+const message3 = `名前は${myProfile[0]}。年齢は${myProfile[1]}`;
+console.log(message3);
+//分割代入 配列の順番通りにいれる
+const [name, age] = myProfile;
+const message4 = `名前は${name}でして年齢は${age}だよ`;
+console.log(message4);
